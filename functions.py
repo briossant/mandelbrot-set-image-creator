2a92c5553d7e0667feb1ce6eecc2f6fd6c1d9461
briossant <crosson.brieuc@gmail.com>
2021-02-10 22:04:47 +0100

    init

--- functions.py
+++ functions.py
@@ -1,8 +1,6 @@
 from PIL import Image
 import os
 
-max_iteration = 80
-
 default_inter = {
     're_start': -0.5,
     're_end': 0,
@@ -11,7 +9,7 @@ default_inter = {
 }
 
 
-def mandelbrot_fct(c):
+def mandelbrot_fct(c, max_iteration):
     z = 0
     n = 0
     while abs(z) <= 2 and n < max_iteration:
@@ -20,9 +18,12 @@ def mandelbrot_fct(c):
     return n
 
 
-def px_calculator(l=400, h=400, name='img', is_save=True, inter=None):
+def px_calculator(l=400, h=400, name='img', is_save=True, inter=None, max_iteration=80, colors=None):
+    if colors is None:
+        colors = [255, 0, 255]
     if inter is None:
         inter = default_inter
+
     print('start')
     img = Image.new('RGB', (l, h))
     for x in range(-l // 2, l // 2):
@@ -31,14 +32,17 @@ def px_calculator(l=400, h=400, name='img', is_save=True, inter=None):
         for y in range(-h // 2, h // 2):
             c = complex(inter['re_start'] + (x / l) * (inter['re_end'] - inter['re_start']),
                         inter['im_start'] + (y / h) * (inter['im_end'] - inter['im_start']))
-            m = mandelbrot_fct(c)
-            r = (int(m * 255 / max_iteration * 2)) % 255
-            g = (int(m * 255 / max_iteration)) % 255
-            b = (255 - int(m * 255 / max_iteration / 2)) % 255
-            img.putpixel((x + l // 2, y + l // 2), (r, g, b))
+            m = mandelbrot_fct(c, max_iteration)
+            r = int((colors[0] * (m / max_iteration)) % 255)
+            g = int((colors[1] * (m / max_iteration)) % 255)
+            b = int((colors[2] * (m / max_iteration)) % 255)
+            img.putpixel((x + l // 2, y + h // 2), (r, g, b))
 
-    img.show()
     if is_save:
         name = "./images/" + name + ".jpg"
         img.save(name)
         os.system("xdg-open " + name)
+    else:
+        print(colors)
+        img.show()
+
--- main.py
+++ main.py
@@ -10,42 +10,72 @@ class Application(tk.Frame):
         super().__init__(master)
         self.master = master
         master.title('Mandelbrot')
-        master.geometry('750x450')
+        master.geometry('700x250')
 
         self.create_widgets()
 
     def create_widgets(self):
+        # entry
         self.size_label = tk.Label(self.master, text='Image size length x height :', font=('bold', 14), pady=10)
         self.size_label.grid(row=0, column=0, sticky=tk.W)
-        self.size_entry = tk.Entry(self.master, width=50)
+        self.size_entry = tk.Entry(self.master, width=15)
         self.size_entry.insert(tk.END, '400x400')
-        self.size_entry.grid(row=0, column=1, columnspan=10)
+        self.size_entry.grid(row=0, column=1, columnspan=10, sticky=tk.W)
 
         self.name_label = tk.Label(self.master, text='Image name :', font=('bold', 14), pady=10)
         self.name_label.grid(row=1, column=0, sticky=tk.W)
-        self.name_entry = tk.Entry(self.master, width=50)
+        self.name_entry = tk.Entry(self.master, width=15)
         self.name_entry.insert(tk.END, 'myImage')
-        self.name_entry.grid(row=1, column=1, columnspan=10)
+        self.name_entry.grid(row=1, column=1, columnspan=10, sticky=tk.W)
+
+        self.iter_label = tk.Label(self.master, text='max iterations :', font=('bold', 14), pady=10)
+        self.iter_label.grid(row=2, column=0, sticky=tk.W)
+        self.iter_entry = tk.Entry(self.master, width=15)
+        self.iter_entry.insert(tk.END, '80')
+        self.iter_entry.grid(row=2, column=1, columnspan=10, sticky=tk.W)
 
         self.re_label = tk.Label(self.master, text='real start x end :', font=('bold', 14), pady=10)
-        self.re_label.grid(row=2, column=0, sticky=tk.W)
-        self.re_entry = tk.Entry(self.master, width=50)
+        self.re_label.grid(row=3, column=0, sticky=tk.W)
+        self.re_entry = tk.Entry(self.master, width=25)
         self.re_entry.insert(tk.END, '0.36x0.37')
-        self.re_entry.grid(row=2, column=1, columnspan=10)
+        self.re_entry.grid(row=3, column=1, columnspan=10, sticky=tk.W)
 
         self.im_label = tk.Label(self.master, text='imaginary start x end :', font=('bold', 14), pady=10)
-        self.im_label.grid(row=3, column=0, sticky=tk.W)
-        self.im_entry = tk.Entry(self.master, width=50)
+        self.im_label.grid(row=4, column=0, sticky=tk.W)
+        self.im_entry = tk.Entry(self.master, width=25)
         self.im_entry.insert(tk.END, '-0.63x-0.64')
-        self.im_entry.grid(row=3, column=1, columnspan=10)
+        self.im_entry.grid(row=4, column=1, columnspan=10, sticky=tk.W)
+
+        # Colors
+
+        self.red_label = tk.Label(self.master, text='red :', font=('bold', 14), pady=10)
+        self.red_label.grid(row=0, column=2, sticky=tk.E)
+        self.red = tk.Scale(self.master, from_=0, to=255, orient='horizontal')
+        self.red.set(255)
+        self.red.grid(row=0, column=3, sticky=tk.E)
+
+        self.green_label = tk.Label(self.master, text='green :', font=('bold', 14), pady=10)
+        self.green_label.grid(row=1, column=2, sticky=tk.E)
+        self.green = tk.Scale(self.master, from_=0, to=255, orient='horizontal')
+        self.green.set(0)
+        self.green.grid(row=1, column=3, sticky=tk.E)
+
+        self.blue_label = tk.Label(self.master, text='blue :', font=('bold', 14), pady=10)
+        self.blue_label.grid(row=2, column=2, sticky=tk.E)
+        self.blue = tk.Scale(self.master, from_=0, to=255, orient='horizontal')
+        self.blue.set(255)
+        self.blue.grid(row=2, column=3, sticky=tk.E)
+
+        # save check
 
         self.check_var = tk.IntVar()
-        self.save_check = tk.Checkbutton(self.master, text='Save image ?', variable=self.check_var, onvalue=1, offvalue=0)
-        self.save_check.grid(row=4, column=0, pady=10)
+        self.save_check = tk.Checkbutton(self.master, text='Save image ?', variable=self.check_var, onvalue=1,
+                                         offvalue=0)
+        self.save_check.grid(row=3, column=3, pady=10)
 
         # Buttons
         self.create_btn = tk.Button(self.master, text="Create image", width=12, command=self.create_img)
-        self.create_btn.grid(row=4, column=1, pady=10)
+        self.create_btn.grid(row=5, column=1, pady=10)
 
     def create_img(self):
         try:
@@ -55,6 +85,8 @@ class Application(tk.Frame):
             re = self.re_entry.get().split('x')
             im = self.im_entry.get().split('x')
 
+            max_iter = int(self.iter_entry.get())
+
             inter = {
                 're_start': float(re[0]),
                 're_end': float(re[1]),
@@ -62,10 +94,14 @@ class Application(tk.Frame):
                 'im_end': float(im[1])
             }
 
-            functions.px_calculator(l=int(L), h=int(H), name=name, is_save=self.check_var.get(), inter=inter)
-        except NameError:
-            functions.px_calculator(is_save=1)
+            colors = [int(self.red.get()),
+                      int(self.green.get()),
+                      int(self.blue.get())]
 
+            functions.px_calculator(l=int(L), h=int(H), name=name, is_save=self.check_var.get(), inter=inter,
+                                    max_iteration=max_iter, colors=colors)
+        except ValueError:
+            functions.px_calculator(is_save=1)
 
 
 l = 4000
